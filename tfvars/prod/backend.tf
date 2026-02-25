2fa624ef6ba4bc8e7b5c741238aac623b14bd8de
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-25 17:09:32 +0530

    actions

---
+++ tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket         = "remote-state-project-annuru-prod" # Replace with your bucket name
+key            = "remote-state.tfstate"       
+region         = "us-east-1"                        
+encrypt        = true
+use_lockfile = true 
